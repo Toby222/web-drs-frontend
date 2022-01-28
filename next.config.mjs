6d54581ad0c4b0297e2301dcf2708e86977a0280
Tobias Berger <14962962+Toby222@users.noreply.github.com>
2022-01-28 11:05:03 +0000

    Next config as module

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -14,3 +17,4 @@ module.exports = {
     return config;
   },
 };
+export default nextConfig;
