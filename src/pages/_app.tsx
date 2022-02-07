0e3197aad33a67f7ed1ae2d571f2296505c9a3ba
Tobias Berger <14962962+Toby222@users.noreply.github.com>
2022-02-07 14:38:57 +0000

    Add title to page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,19 @@
 import "../styles.scss";
 
-export { default } from "next/app";
+import App from "next/app";
+import Head from "next/head";
+
+class CustomApp extends App {
+  render() {
+    return (
+      <>
+        <Head>
+          <title key="page-title">DRS Chat</title>
+        </Head>
+        {super.render()}
+      </>
+    );
+  }
+}
+
+export default CustomApp;
